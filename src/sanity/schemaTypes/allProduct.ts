f3ca2e820732356531c1230f44c4df7c2a1235db
Ghulam Akber <160354667+Akber261986@users.noreply.github.com>
2025-01-27 00:13:10 +0500

    image, schema and json updated

---
+++ src/sanity/schemaTypes/allProduct.ts
@@ -0,0 +1,75 @@
+import { defineField, defineType } from "sanity";
+
+export const allProducts = defineType({
+  name: "allproducts",
+  title: "All Products",
+  type: "document",
+  fields: [
+    defineField({
+      name: "name",
+      title: "Name",
+      type: "string",
+    }),
+    defineField({
+      name: "price",
+      title: "Price",
+      type: "number",
+    }),
+    defineField({
+      name: "discountedPrice",
+      title: "Discounted Price",
+      type: "number",
+    }),
+    defineField({
+      name: "productcode",
+      title: "Product Code",
+      type: "string",
+    }),
+    defineField({
+      name: "rating",
+      title: "Rating",
+      type: "number",
+    }),
+    defineField({
+      name: "category",
+      title: "Category",
+      type: "array",
+      of: [{ type: "string" }],
+    }),
+    defineField({
+      name: "isSale",
+      title: "Is Sale?",
+      type: "boolean",
+    }),
+    defineField({
+      name: "description",
+      title: "Description",
+      type: "text",
+    }),
+    defineField({
+      name: "colors",
+      title: "Colors",
+      type: "array",
+      of: [{ type: "string" }],
+    }),
+    defineField({
+      name: "tags",
+      title: "Tags",
+      type: "array",
+      of: [{ type: "string" }],
+    }),
+    defineField({
+      name: "stocklevel",
+      title: "Stock Level",
+      type: "number",
+    }),
+    defineField({
+        name: "image",
+        title: "Image",
+        type: "image",
+        options: {
+          hotspot: true,
+        },
+      }),
+  ],
+});
